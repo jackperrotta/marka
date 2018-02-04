70c024fb78e652b779e697378c2e19e4c0d149aa
Vitris <tug24657@temple.edu>
2018-02-04 15:18:41 -0500

    mvc structure

---
+++ common/config.php
@@ -0,0 +1,9 @@
+<?php
+    //this is for the database
+    $dsn = 'mysql:host=localhost;dbname=marka';
+    $username = 'marka';
+    $password = 'password';
+
+    //this path can be used in place of relative references in HTML
+    $base_path = 'http://localhost/marka';
+?>
