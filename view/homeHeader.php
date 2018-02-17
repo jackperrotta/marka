f592288e716dc5c42f73f08aedc417bee10bb85c
Vitris <tug24657@temple.edu>
2018-02-17 15:16:33 -0500

    set up nav and started home page

---
+++ view/homeHeader.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="<?php echo $base_path;?>/img/marka-logo.png">
+
+    <title>Marka - Easy, Cashless, & Convenient Beach Tags</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="<?php echo $base_path;?>/styles/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles -->
+    <link href="<?php echo $base_path;?>/styles/main.css" rel="stylesheet">
+  </head>
+  <body>
+
+    <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="<?php echo $base_path;?>">
+      <img src="<?php echo $base_path;?>/img/marka-logo.png" width="30" height="30" alt="">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbar">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="<?php echo $base_path;?>">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="<?php echo $base_path;?>#about">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"></a>
+        </li>
+      </ul>
+    </div>
+  </nav>
