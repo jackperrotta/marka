28eafd8e5335fa3ebcde3086faf85f6ac9c317e1
Vitris <tug24657@temple.edu>
2018-04-25 14:37:46 -0400

    last min updates

--- common/config.php
+++ common/config.php
@@ -1,11 +1,11 @@
 <?php
     //this is for the class server database
-    $dsn = 'mysql:host=localhost;dbname=db_tug24657';
-    $username = 'tug24657';
-    $password = '2JvA6ZVA';
+    // $dsn = 'mysql:host=localhost;dbname=db_tug24657';
+    // $username = 'tug24657';
+    // $password = '2JvA6ZVA';
 
     //this path can be used in place of relative references in HTML
-    $base_path = 'http://misdemo.temple.edu/tug24657/marka';
+    // $base_path = 'http://misdemo.temple.edu/tug24657/marka';
 
     //this is for the local database
     // $dsn = 'mysql:host=localhost;dbname=marka';
@@ -14,4 +14,10 @@
 
     //this path can be used in place of relative references in HTML
     // $base_path = 'http://localhost/marka';
+
+    $dsn = 'mysql:host=monitoringapp.c6ufur3cegqo.us-west-2.rds.amazonaws.com:3306;dbname=monitoringApp';
+    $username = 'admin';
+    $password = 'password';
+
+    $base_path = 'https://licensekeysentry.com';
 ?>
